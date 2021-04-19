004a0a55867ed09bd86d841fd74701779caca21d
TarekAlhalabi <tarekalhalaby946@gmail.com>
2021-04-19 10:46:51 +0300

    update the home page ads images

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -8,10 +8,10 @@
   <section id="carouselExampleControls" class="carousel slide" data-ride="carousel">
     <div class="carousel-inner">
       <div class="carousel-item active">
-        <img class="d-block w-100" src="../assets/images/ad1.png" alt="First slide">
+        <img class="d-block w-100" src="../assets/images/banner1.jpg" alt="First slide">
       </div>
       <div class="carousel-item">
-        <img class="d-block w-100" src="../assets/images/ad2.jpg" alt="Second slide">
+        <img class="d-block w-100" src="../assets/images/banner2.jpg" alt="Second slide">
       </div>
     </div>
     <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
