df9158fc3288763fa36bd4290b508c8e786ae478
TarekAlhalabi <tarekalhalaby946@gmail.com>
2021-04-20 17:50:46 +0300

    complete the user orders history page

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Laptop;
 
 class Order extends Model
 {
     use HasFactory;
+
+    /*
+    * Get Order Laptops
+    */
+    public function laptops(){
+        return $this->belongsToMany(Laptop::class)->with(['price']);
+    }
 }
--- resources/views/user/orders.blade.php
+++ resources/views/user/orders.blade.php
@@ -10,70 +10,73 @@
   
       <div class="row justify-content-center">
         <div class="col-12 col-md-8 col-lg-6">
-          <!--Start Order Item-->
-          <div class="item row align-items-center shadow py-2 px-1 mb-2">
-            <div class="col-12">
-              <span class="badge badge-secondary rounded-pill">
-                <i class="lni lni-calendar"></i> 
-                <span>22-2-2021 12:57</span>
-              </span>
-              <span class="badge badge-success rounded-pill float-left">
-                <i class="lni lni-checkmark-circle"></i> 
-                <span>منجز</span>
-              </span>
-            </div>
-            <div class="col-12 mt-2">
-              <p>المنتجات المطلوبة</p>
-              <div class="products row mb-3">
-                <div class="col-12 row mb-3">
-                  <div class="col-3">
-                    <img src="../assets/images/laptop1.jpg" class="img-fluid">
-                  </div>
-                  <div class="col-9">
-                    <p class="font-weight-bold mb-1">
-                      <a href="laptop.html" class="text-dark">Asus AMD3 99887 nivedia</a>
-                    </p>
-                    <small>السعر عند الطلب</small>
-                    <span class="text-success font-weight-bold"><bdi>s.p</bdi> 125000</span>
-                  </div>
-                </div>
+
+          @forelse ($orders as $order)
+              
+            <div class="item row align-items-center shadow py-2 px-1 mb-2">
+              <div class="col-12">
+                <span class="badge badge-secondary rounded-pill">
+                  <i class="lni lni-calendar"></i> 
+                  <span>{{ $order->created_at }}</span>
+                </span>
+
+                @if ($order->status)
+                  <span class="badge badge-success rounded-pill float-left">
+                    <i class="lni lni-checkmark-circle"></i> 
+                    <span>منجز</span>
+                  </span>
+                @else
+                  <span class="badge badge-danger rounded-pill float-left">
+                    <i class="lni lni-close"></i> 
+                    <span>قيد المتابعة</span>
+                  </span>
+                @endif
+
               </div>
-              <b>قيمة الطلب</b>
-              <h3 class="d-inline-block text-danger"><bdi>s.p</bdi> 125000</h3>
-            </div>
-          </div>
-          <!--End Order Item-->
-          <div class="item row align-items-center shadow py-2 px-1 mb-2">
-            <div class="col-12">
-              <span class="badge badge-secondary rounded-pill">
-                <i class="lni lni-calendar"></i> 
-                <span>22-2-2021 12:57</span>
-              </span>
-              <span class="badge badge-success rounded-pill float-left">
-                <i class="lni lni-checkmark-circle"></i> 
-                <span>منجز</span>
-              </span>
-            </div>
-            <div class="col-12 mt-2">
-              <p>المنتجات المطلوبة</p>
-              <div class="products row mb-3">
-                <div class="col-12 row mb-3">
-                  <div class="col-3">
-                    <img src="../assets/images/laptop1.jpg" class="img-fluid">
-                  </div>
-                  <div class="col-9">
-                    <p class="font-weight-bold mb-1">
-                      <a href="laptop.html" class="text-dark">Asus AMD3 99887 nivedia</a>
-                    </p>
-                    <small>السعر عند الطلب</small>
-                    <span class="text-success font-weight-bold"><bdi>s.p</bdi> 125000</span>
-                  </div>
+              <div class="col-12 mt-2">
+                <p>المنتجات المطلوبة</p>
+                <div class="products row mb-3">
+
+                  @php
+                      $laptops = $order->laptops;
+                  @endphp
+
+                  @foreach ($laptops as $laptop)
+                    <div class="col-12 row mb-3">
+                      <div class="col-3">
+                        <img src="../assets/images/laptop1.jpg" class="img-fluid">
+                      </div>
+                      <div class="col-9">
+                        <p class="font-weight-bold mb-1">
+                          <a href="laptops/{{ $laptop->id }}/{{ $laptop->name }}" class="text-dark">{{ $laptop->name }}</a>
+                        </p>
+                        <small>السعر عند الطلب</small>
+                        <span class="text-success font-weight-bold"><bdi>s.p</bdi> {{ $laptop->price }}</span>
+                      </div>
+                    </div>
+                  @endforeach
+
                 </div>
+                <b>قيمة الطلب</b>
+                <h3 class="d-inline-block text-danger"><bdi>s.p</bdi> {{ $order->total_price }}</h3>
               </div>
-              <b>قيمة الطلب</b>
-              <h3 class="d-inline-block text-danger"><bdi>s.p</bdi> 125000</h3>
             </div>
-          </div>
+
+          @empty
+
+            @auth
+              <div class="alert alert-secondary">لم تقم بطلب أي شيء حتى الآن</div>
+            @endauth
+
+            @guest
+              <div class="alert alert-secondary mt-5 mb-5">
+                قم بإنشاء حسابك لدينا واطلب من متجرنا كما تريد
+                <a href="/register">إنشاء حساب</a>
+              </div>
+            @endguest
+              
+          @endforelse
+
         </div>
       </div>
 
